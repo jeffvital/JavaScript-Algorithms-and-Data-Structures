19e639ec58e14c4bb81c5e569cc54e8996c5d4a7
jeffvital <Jeffersonjvs@outlook.com>
2023-04-18 22:05:55 -0300

    best practice for varibles

--- Basic JavaScript/understanding_Case_Sensitivity_in_variable.js
+++ Basic JavaScript/understanding_Case_Sensitivity_in_variable.js
@@ -1,1 +1,7 @@
-// Explore Differences Between the var and let Keywords
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
