8d6bb1854dce3ad4397ee6518a7c8166a5c526dc
jeffvital <Jeffersonjvs@outlook.com>
2023-04-19 16:04:22 -0300

    remainder operator

---
+++ Basic JavaScript/finding_a_remainder_in_javascript.js
@@ -0,0 +1,6 @@
+const remainder = 11 % 3;
+
+console.log(remainder)
+
+// Note: the remainder operator is  sometimes incorrectly referred to as the modulus operator. 
+// It is very similar to modulus, but does not work properly with negative numbers.
