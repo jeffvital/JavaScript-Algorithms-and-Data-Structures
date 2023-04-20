c5a37502e277520831efaeb1d40753cc88fa37ee
jeffvital <Jeffersonjvs@outlook.com>
2023-04-20 15:55:42 -0300

    like += 5 increse, -= 5 decrease.

---
+++ Basic JavaScript/compound_assignment_with_augmented_subtraction.js
@@ -0,0 +1,14 @@
+// Like the += operator, -= subtracts a number from a variable
+
+// let myVar = 7;
+// myVar = myVar - 5;
+
+// console.log(myVar) // 2
+
+let a = 11;
+let b = 9;
+let c = 3;
+
+a -= 6;
+b -= 15;
+c -= 1;
