e654b1cdce2f817140453345be04e6eafdc567e8
jeffvital <Jeffersonjvs@outlook.com>
2023-04-24 15:47:14 -0300

    cannot change declared strings

---
+++ Basic JavaScript/understanding_string_immutability.js
@@ -0,0 +1,8 @@
+let myStr = "Jello World";
+
+myStr = "Hello World";
+
+// Não é possível modificar da seguinte maneira
+// myStr[0] por que uma vez que a string
+// é declarada não possivel modifica-la
+// mas é possivel fazer uma reatribuição. como no primeiro caso.
