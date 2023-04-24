fa7716ccbdc52ec3f1bad7780e632a53bdcd790e
jeffvital <Jeffersonjvs@outlook.com>
2023-04-24 16:20:36 -0300

    lastName[lastName.length - 1]

---
+++ Basic JavaScript/use_bracket_notation_to_finding_the_last_character_in_a_string.js
@@ -0,0 +1,8 @@
+const lastName = "Lovelace";
+const lastLetterOfLastName = lastName[lastName.length - 1];
+
+console.log(lastLetterOfLastName);
+
+// Retorna o index da ultima letra
+// O index é o tamanho da string - 1
+// se a string tem 8 letras o index da ultima letra é 7
