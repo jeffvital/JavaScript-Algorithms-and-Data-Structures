2949bb12bb51c5cfb451e41a3490e0a5a53ac415
jeffvital <Jeffersonjvs@outlook.com>
2023-05-04 15:14:41 -0300

    greater than or equal to operator

---
+++ Basic JavaScript/comparison_with_the_greater_than_or_equal_to_operator.js
@@ -0,0 +1,11 @@
+function testGreaterThanOrEqual(val) {
+    if(val >= 20) {
+        return "20 or Over";
+    }
+    if(val >= 10) {
+        return "10 or Over";
+    }
+    return "Less than 10"
+}
+
+console.log(testGreaterThanOrEqual(0));
