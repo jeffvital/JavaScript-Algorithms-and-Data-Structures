1a9a2300ee90016d3d863c11e6eab4c04fea3f61
jeffvital <Jeffersonjvs@outlook.com>
2023-05-09 16:10:19 -0300

    swtich case

---
+++ Basic JavaScript/selecting_from_many_options_with_switch_statements.js
@@ -0,0 +1,25 @@
+function caseInSwitch(val) {
+  let answer = "";
+  switch (val) {
+    case 1:
+      answer = "alpha";
+      break;
+    case 2:
+      answer = "beta";
+      break;
+    case 3:
+      answer = "gamma";
+      break;
+    case 4:
+      answer = "delta";
+      break;
+    default:
+      answer = "unknown";
+      break;
+  }
+  return answer;
+}
+
+const result = caseInSwitch(4);
+
+console.log(result);
