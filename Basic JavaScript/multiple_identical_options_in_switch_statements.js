f6cc9a3888f15f817c611401fcef3e181bf4ad3b
jeffvital <Jeffersonjvs@outlook.com>
2023-05-10 16:10:56 -0300

    sequential cases

---
+++ Basic JavaScript/multiple_identical_options_in_switch_statements.js
@@ -0,0 +1,27 @@
+function sequentialSizes(val) {
+  let answer = "";
+  switch (val) {
+    case 1:
+    case 2:
+    case 3:
+      answer = "Low";
+      break;
+    case 4:
+    case 5:
+    case 6:
+      answer = "Mid";
+      break;
+    case 7:
+    case 8:
+    case 9:
+      answer = "High";
+      break;
+    default:
+      answer = "Unknown";
+      break;
+  }
+  return answer;
+}
+
+let result = sequentialSizes(1);
+console.log(result);
