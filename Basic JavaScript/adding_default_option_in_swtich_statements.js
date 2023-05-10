90145ced9d0ad63119d40cdf9a71666367ac0cc3
jeffvital <Jeffersonjvs@outlook.com>
2023-05-10 16:02:43 -0300

    switch case with default statement

---
+++ Basic JavaScript/adding_default_option_in_swtich_statements.js
@@ -0,0 +1,21 @@
+function swtichOfStuff(val) {
+  let answer = "";
+  switch (val) {
+    case "a":
+      answer = "apple";
+      break;
+    case "b":
+      answer = "bird";
+      break;
+    case "c":
+      answer = "cat";
+      break;
+    default:
+      answer = "stuff";
+      break;
+  }
+  return answer;
+}
+
+let result = swtichOfStuff("c");
+console.log(result);
