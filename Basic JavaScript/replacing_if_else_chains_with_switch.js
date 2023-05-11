5d979ac8938e71b107b8eb574103c3f64c5bf1a4
jeffvital <Jeffersonjvs@outlook.com>
2023-05-11 15:55:11 -0300

    replacing if else with switch

---
+++ Basic JavaScript/replacing_if_else_chains_with_switch.js
@@ -0,0 +1,25 @@
+function chainToSwtich(val) {
+  let answer = "";
+  switch (val) {
+    case "bob":
+      answer = "Marley";
+      break;
+    case 42:
+      answer = "The Answer";
+      break;
+    case 1:
+      answer = "There is no #1";
+      break;
+    case 99:
+      answer = "Missed me by this much!";
+      break;
+    case 7:
+      answer = "Ate Nine";
+      break;
+  }
+  return answer;
+}
+
+let result = chainToSwtich(7);
+
+console.log(result);
