1b983f66edfca57870b950c91ced213eb898f5fc
jeffvital <Jeffersonjvs@outlook.com>
2023-05-21 18:34:20 -0300

    manipulating complex objects

---
+++ Basic JavaScript/manipulating_complex_objects.js
@@ -0,0 +1,15 @@
+const myMusic = [
+  {
+    artist: "Billy Joel",
+    title: "Piano Man",
+    release_year: "1973",
+    formats: ["CD", "8T", "LP"], // array as value
+  },
+  {
+    artist: "Corey Taylor",
+    title: "All Hope is Gone",
+    release_year: 2008,
+    formats: ["CD", "8T", "LP"], // array as value
+    platinum: true,
+  },
+];
