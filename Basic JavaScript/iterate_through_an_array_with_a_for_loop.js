4560ee56b10323017dbfa76b5052179c4b58b787
jeffvital <Jeffersonjvs@outlook.com>
2023-05-29 22:55:31 -0300

    adding up array elements to a variable

--- Basic JavaScript/iterate_through_an_array_with_a_for_loop.js
+++ Basic JavaScript/iterate_through_an_array_with_a_for_loop.js
@@ -1,9 +1,6 @@
-const myArray = [2, 3, 4, 5, 6];
+const myArr = [2, 3, 4, 5, 6];
 let total = 0;
-for (let i = 0; i < myArray.length; i++) {
-  total.push[i];
+for (let i = 0; i < myArr.length; i++) {
+  total += myArr[i];
 }
-
 console.log(total);
-
-// Working on it
