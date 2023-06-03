842c366d64c2255db1ce3a18c9f574921015c2f7
jeffvital <Jeffersonjvs@outlook.com>
2023-06-02 23:07:44 -0300

    working on the code

---
+++ Basic JavaScript/profile_lookup.js
@@ -0,0 +1,40 @@
+// Setup
+const contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
+
+function lookUpProfile(name, prop) {
+  if (name === contacts[firstName] && prop === contacts[lastName]) {
+    return prop;
+  } else if (name !== contacts[firstName]) {
+    return `No such contact`;
+  } else if (prop !== contacts[lastName]) {
+    return `No such property`;
+  }
+}
+
+let result = lookUpProfile("Akira", "likes");
+console.log(result);
