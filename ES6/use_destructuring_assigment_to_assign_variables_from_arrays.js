03d3887c60f0ee05b5a26ce1e5159df66473cf1a
jeffvital <Jeffersonjvs@outlook.com>
2023-06-14 16:41:14 -0300

    destructuring assigment to assign from arrays

---
+++ ES6/use_destructuring_assigment_to_assign_variables_from_arrays.js
@@ -0,0 +1,11 @@
+// const [a, b] = [1, 2, 3, 4, 5, 6];
+// console.log(a, b);
+
+// const [a, b, , , c] = [1, 2, 3, 4, 5, 6];
+// console.log(a, b, c);
+
+let a = 8,
+  b = 6;
+
+[a, b] = [b, a];
+console.log(a, b);
