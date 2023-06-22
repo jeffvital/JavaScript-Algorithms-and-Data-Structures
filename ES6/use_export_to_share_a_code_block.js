11168172a70980b94960911cd02851255bda47ae
jeffvital <Jeffersonjvs@outlook.com>
2023-06-22 12:25:18 -0300

    export to share a code block

---
+++ ES6/use_export_to_share_a_code_block.js
@@ -0,0 +1,11 @@
+const uppercaseString = (string) => {
+  return string.toUpperCase();
+};
+
+export { uppercaseString };
+
+const lowercaseString = (string) => {
+  return string.toLowercaseString();
+};
+
+export { lowercaseString };
