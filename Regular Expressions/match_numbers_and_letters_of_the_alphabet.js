d9228964e5d720338040c63e0feca462d479fc92
jeffvital <Jeffersonjvs@outlook.com>
2023-06-27 14:43:50 -0300

    match numbers and letters of the alphabet

---
+++ Regular Expressions/match_numbers_and_letters_of_the_alphabet.js
@@ -0,0 +1,10 @@
+// let jennyStr = "Jenny8675309";
+// let myRegex = /[a-z0-9]/gi;
+// jennyStr.match(myRegex);
+
+let quoteSample = "Blueberry 3.141592653s are delicious";
+
+let myRegex = /[h-s2-6]/gi;
+let result = quoteSample.match(myRegex);
+
+console.log(result);
