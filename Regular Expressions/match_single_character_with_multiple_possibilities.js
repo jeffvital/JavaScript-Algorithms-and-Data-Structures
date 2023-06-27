2877407b45c0b4d0939e757044621ba030d55c86
jeffvital <Jeffersonjvs@outlook.com>
2023-06-27 14:25:45 -0300

    match single character with multiple possibilities

---
+++ Regular Expressions/match_single_character_with_multiple_possibilities.js
@@ -0,0 +1,19 @@
+let quoteSample =
+  "Beware of bugs in the above code; I have only proved it correct, not tried it.";
+
+let vowelRegex = /[aeiou]/gi;
+let result = quoteSample.match(vowelRegex);
+
+console.log(result);
+
+// EXEMPLE OF USE
+
+// let bigStr = "big";
+// let bagStr = "bag";
+// let bugStr = "bug";
+// let bogStr = "bog";
+// let bgRegex = /b[aiu]g/;
+// bigStr.match(bgRegex);
+// bagStr.match(bgRegex);
+// bugStr.match(bgRegex);
+// bogStr.match(bgRegex);
